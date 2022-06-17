e5b0781b40f1731d09863293f4b4e4a04ffd3d1c
Binay kumar lohani <74678793+bin06012001@users.noreply.github.com>
2022-06-17 18:42:19 +0530

    Time: 375 ms (11.44%), Space: 36.5 MB (97.76%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,33 @@
+
+#define imax INT_MAX
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+         vector<int>dis(n+1,imax);
+            dis[k]=0;
+            for(int i=1;i<n;++i)
+            {
+                    //cout<<"P";
+                    for(int j=0;j<times.size();++j)
+                    {
+                            //cout<<"R";
+                            int u=times[j][0];
+                            int v=times[j][1];
+                            int w=times[j][2];
+                            if(dis[u]!=imax and dis[u]+w<dis[v])
+                            {
+                                    dis[v]=w+dis[u];
+                            }
+                    }
+            }
+            int answer=0;
+            for(int i=1;i<=n;++i)
+            {
+                    // cout<<"res";
+                         if(dis[i]==imax){return -1;}//cout<<"run";
+                         answer=max(answer,dis[i]);
+            }
+            return answer;
+    }
+};
