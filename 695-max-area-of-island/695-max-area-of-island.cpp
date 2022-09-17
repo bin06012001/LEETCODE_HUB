bb2e56ab179f3a7cdf11f5107b76ef93cb688df9
Binay kumar lohani <74678793+bin06012001@users.noreply.github.com>
2022-09-17 12:58:38 +0530

    Time: 18 ms (93.05%), Space: 23.3 MB (64.28%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-       int ans = 0;
-        n = grid.size(), m = grid[0].size();
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < m; j++)
-                if (grid[i][j]) ans = max(ans, trav(i, j, grid));
-        return ans; 
+    void dfs(int i, int j, int n, int m, vector<vector<int>>& grid, int& currArea){
+        
+        //Handling Invalid Cases
+        
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == 0){
+            return;
+        }
+        
+        currArea += 1;
+        grid[i][j] = 0;
+        
+        dfs(i+1,j,n,m,grid,currArea);
+        dfs(i,j+1,n,m,grid,currArea);
+        dfs(i-1,j,n,m,grid,currArea);
+        dfs(i,j-1,n,m,grid,currArea);
     }
     
-    private:
-    int n, m;
-    int trav(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0;
-        grid[i][j] = 0;
-        return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        int maxArea = 0; //For maximum Area
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i = 0; i<n; i++){
+            
+            for(int j = 0; j<m; j++){
+                
+                if(grid[i][j] == 1){
+                    int currArea = 0; //For current Area
+                    dfs(i,j,n,m,grid,currArea);
+                    maxArea = max(maxArea, currArea); //Updating Current Area with Maximum
+                }
+                
+            }
+        }
+        
+        return maxArea;
+        
     }
 };
